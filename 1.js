59c6b20fa9a69d4487aafe4a22570bd552c07eee
ritju23 <maheshwari.ritu1997@gmail.com>
2019-07-23 23:16:14 +0530

    for loop added

--- 1.js
+++ 1.js
@@ -1,10 +1,27 @@
+// if else
 function A (name){
-    
+    let a;
     if( name === "true"){
-    var a = "hello ritu";  
+    a = "hello ritu";  
     } 
-    else 
-     var a = "who are you?";
- 
+    else {
+     a = "who are you?";
+    }
 console.log(a) ;}
 A("true");
+
+var x=1; 
+var y=2;
+if (x === 1){
+    var x=10;
+    let y=20;
+    console.log(x);
+    console.log(y);
+}
+console.log(x);
+console.log(y);
+
+// for loop
+for (let i=1; i<=5; i++){
+   setTimeout(function(){console.log(i);},1000);
+}
