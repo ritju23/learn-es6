f0170221cf7024a034459e2dc2c2b91358891c73
ritju23 <maheshwari.ritu1997@gmail.com>
2019-07-24 00:16:49 +0530

    const commit

---
+++ const.js
@@ -0,0 +1,15 @@
+let num1;
+const num2 = 10;
+const obj1 = {
+    name:"rituu"
+};
+console.log(num1);
+console.log(obj1.name);
+console.log(obj1);
+obj1.name ="menu";
+console.log(obj1.name);
+
+function A (){
+    for(var i=15; i>=num2; i--)
+    console.log(i);}
+    A();
